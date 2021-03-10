f6aee84bd69dee025d3e6a4d5c9ca362a024a190
Florian Schaeffeler <florian.schaeffeler@avira.com>
2021-03-10 17:07:23 +0100

    Added editorconfig adapted CMakeLists.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
 #include <fmt/format.h>
-#include <cppitertools/itertools.hpp>
 
-int fib(int n) {
-	int a = 0, b = 1;
+int fib(int n)
+{
+    int a = 0, b = 1;
 
-	for ([[maybe_unused]] int i : iter::range(n)) {
-		auto tmp = b;
-		b = a + b;
-		a = tmp;
-	}
+    for ([[maybe_unused]] int i : iter::range(n))
+    {
+        auto tmp = b;
+        b = a + b;
+        a = tmp;
+    }
 
-	return a;
-}
+    return a;
+}#
 
-int main() {
-	for (int i : iter::range(15)) {
-		fmt::print("fib({}) = {}\n", i, fib(i));
-	}
+int main()
+{
+    for (int i : iter::range(15))
+    {
+        fmt::print("fib({}) = {}\n", i, fib(i));
+    }
 }
